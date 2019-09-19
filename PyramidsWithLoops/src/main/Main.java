fc249cef2f05b4d8327c268f0332c429002c3afa
ramen2spicy <ramen2spicy@tianyangs-air.wifi.uc.edu>
2019-09-19 09:20:54 -0400

    I think I got it

--- PyramidsWithLoops/src/main/Main.java
+++ PyramidsWithLoops/src/main/Main.java
@@ -11,15 +11,27 @@
 */
 package main;
 
-public class Main {
 
-	public static void main(String[] args) {
+public class Main {
+	 public static void main(String[] args) {
+
 		int lines = 5;
-		for (int i = 0; i < lines; i++) {
-			for (int j = 0; j < i+1; j++) {
-				System.out.print(j);
+		String space = " ";
+		int k = 1;
+		int space_control = lines;
+		for (int i = 1; i < lines+1; i++, space_control--, k = k+2){
+			String times = Integer.toString(i);
+				System.out.println(space.repeat(space_control) + times.repeat(k));
 			}
-			System.out.println("");
-		}
+		
+		
+		/*	for (int i = 0; i < lines; i++){
+					for (int j = 0; j < i+1; j++) {
+						System.out.print(space+j);
+				System.out.println(space.repeat(lines));
+				}
+				System.out.println("");*/
+			}
+		
+	
 	}
-}
